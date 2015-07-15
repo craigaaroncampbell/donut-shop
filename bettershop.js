fc776899ef1481dd3a18476dc76f79cd5d8e2826
Craig Campbell <craig.campbell8@gmail.com>
2015-07-15 14:21:07 -0700

    got the update to work

--- bettershop.js
+++ bettershop.js
@@ -15,38 +15,31 @@ this.storeLocation = storeLocation;
 this.minCustPerHr = minCustPerHr;
 this.maxCustPerHr = maxCustPerHr;
 this.avgDonutsPerCust = avgDonutsPerCust;
+this.hourlyDonuts = [];
+this.dailyDonuts = 0;
 };
 
 //method: calculate customers per hour
 Shop.prototype.calculateHourlyCustomers = function(){
-  this.hourlyCustomers = Math.floor(Math.random() * ( this.maxCustPerHr - this.minCustPerHr + 1 )) + this.minCustPerHr;
-  return this.hourlyCustomers;
+  var hourlyCustomers = Math.floor(Math.random() * ( this.maxCustPerHr - this.minCustPerHr + 1 )) + this.minCustPerHr;
+  return hourlyCustomers;
 };
 
 //method: calculate donuts per hour
 //multiply hourly customers by avg donuts per customer. round it so that it is a whole number
 //push each hour's donuts into an array so it keeps track of the donuts each hour - recalculate 11 times using a loop
 Shop.prototype.calculateHourlyDonuts = function(){
-
-  this.hourlyDonuts = [];
   for (var i = 0; i < this.hours; i++){
-    this.calculateHourlyCustomers();
-    this.donutsThisHour = Math.round(this.hourlyCustomers * this.avgDonutsPerCust);
-    this.hourlyDonuts.push(this.donutsThisHour);
+    var hourlyCustomers = this.calculateHourlyCustomers();
+    var donutsThisHour = Math.round(hourlyCustomers * this.avgDonutsPerCust);
+    this.hourlyDonuts.push(donutsThisHour);
+    this.dailyDonuts += donutsThisHour;
   }
-  return this.hourlyDonuts;
+
 };
 
 //method : calculate donuts per day - needs number per hour for each hour
   //so.... first calculate donuts per hour ... do this 11 times.. then add up the results to get total
-Shop.prototype.calculateDailyDonuts = function(){
-  this.calculateHourlyDonuts();
-  this.dailyDonuts = 0
-    for (var i = 0; i < this.hours; i++){
-      this.dailyDonuts += this.hourlyDonuts[i];
-    }
-    return this.dailyDonuts;
-};
 
 //method : do all the following:
     //create new element for table - tr
@@ -57,7 +50,6 @@ Shop.prototype.calculateDailyDonuts = function(){
 
 
 Shop.prototype.makeTable = function(){
-  this.calculateDailyDonuts();
   //create table row, table header, and td elements to put data in
   var tr = document.createElement('tr');
   tr.setAttribute("id", this.storeLocation)
@@ -68,26 +60,27 @@ Shop.prototype.makeTable = function(){
 
   //insert store location into the new row
   th.textContent = this.storeLocation;
-  th.setAttribute('class', this.storeLocation);
+  // th.setAttribute('class', this.storeLocation);
   tr.appendChild(th);
 
   // loop to insert hourly donuts into table
   for (var i = 0; i < this.hours; i++){
     var td = document.createElement('td');
     td.textContent = this.hourlyDonuts[i];
-    td.setAttribute('class', this.storeLocation);
+    // td.setAttribute('class', this.storeLocation);
     tr.appendChild(td);
   }
 
   //insert daily donuts total into table
   var td = document.createElement('td');
   td.textContent = this.dailyDonuts;
-  td.setAttribute('class', this.storeLocation);
+  // td.setAttribute('class', this.storeLocation);
   tr.appendChild(td);
 
   //add the new row to the table
   tbl.appendChild(tr);
 };
+
 
 
 var locationArray = [];
@@ -104,6 +97,7 @@ locationArray.push(new Shop("Ballard", 8, 58, 3.75));
 
 //call method that adds data to table for each store location
 for (var i = 0; i < locationArray.length; i++){
+  locationArray[i].calculateHourlyDonuts();
   locationArray[i].makeTable();
 }
 
@@ -132,16 +126,32 @@ var newStore = function(){
 
     if (found === false){
       locationArray.push(new Shop(newLocation, newMin, newMax, newAvg));
+      locationArray[locationArray.length - 1].calculateHourlyDonuts();
       locationArray[locationArray.length - 1].makeTable();
     }
     if (found === true){
+
       var foundRow = document.getElementById(locationArray[index].storeLocation);
-      var foundRowParent = foundRow.parentNode;
-      foundRowParent.removeChild(foundRow);
+      var childrenToReplace = foundRow.childNodes;
+
 
       locationArray[index] = new Shop(newLocation, newMin, newMax, newAvg);
-      locationArray[index].makeTable();
-      }
+      locationArray[index].calculateHourlyDonuts();
+
+      for (var i = 1; i < childrenToReplace.length; i++){
+      childrenToReplace[i].textContent = locationArray[index].hourlyDonuts[i-1];
+
+      // locationArray[index].calculateHourlyDonuts();
+      // console.log(childrenToReplace[i])
+       // = locationArray[index].calculateHourlyDonuts();
+     }
+     childrenToReplace[childrenToReplace.length - 1].textContent = locationArray[index].dailyDonuts;
+      // var foundRowParent = foundRow.parentNode;
+      // foundRowParent.removeChild(foundRow);
+      // locationArray[index] = new Shop(newLocation, newMin, newMax, newAvg);
+
+      // locationArray[index].makeTable();
+    }
 };
 
 var newEl = document.getElementById('addStore');
